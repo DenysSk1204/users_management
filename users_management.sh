5614887ad6b4b31a7fff213e74fa93e2b77b4793
Denys SK <sktrnk.denys@gmail.com>
2024-12-15 13:02:48 +0200

    Moved menu to main file

--- users_management.sh
+++ users_management.sh
@@ -2,4 +2,24 @@
 
 source functions.sh
 
-main_menu
+PS3="Select the number of operation: "
+
+    select OPT in "Create user" "Update user" "Delete user" "Quit"; do
+        case $OPT in
+            "Create user")
+                echo "The user has been created";;
+
+            "Update user")
+                echo "The user has been updated";;
+
+            "Delete user")
+                echo "The user has been deleted";;
+
+            "Quit")
+                echo "Bye!" 
+                break;;
+
+            *)
+                echo "Invalid option... try again";;
+        esac
+    done
